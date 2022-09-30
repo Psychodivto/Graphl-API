cfd13e6b9687209ae43f46118ab7052270bd9a74
Psychodivto <condediego48@gmail.com>
2022-09-30 02:22:03 -0500

    test

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,29 +1,15 @@
 import graphene
-<<<<<<< HEAD
 import graphql_jwt
 
 import links.schema
 import users.schema
 
-
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(links.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
-
 schema = graphene.Schema(query=Query, mutation=Mutation)
-=======
-
-import links.schema
-
-
-class Query(links.schema.Query, graphene.ObjectType):
-    pass
-
-
-schema = graphene.Schema(query=Query)
->>>>>>> develop
